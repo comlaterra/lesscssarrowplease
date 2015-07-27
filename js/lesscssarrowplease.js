8931f3d5f9259a089685a60a5e0baa14d7282e36
Edmon <comlaterra@gmail.com>
2015-07-27 22:33:54 +0200

    Added the text and changing content

--- js/lesscssarrowplease.js
+++ js/lesscssarrowplease.js
@@ -12,19 +12,31 @@ function lesscssarrowplease(){
 			this.modBox(input.id);
 		}.bind(this),false);
 	}
+
 	this.getCheckedPlaceValue = function(){
 		var checkedInput = document.querySelectorAll("input[name='position']:checked")[0];
 		return checkedInput?checkedInput.value:'top';
 	};
+
 	this.modBox = function(){
 	  less.modifyVars({
 	    '@arrow-border-color' : inputsMapping.border_color.value,
 	    '@color': inputsMapping.color.value,
 	    '@border-width': inputsMapping.border_width.value,
 	    '@arrow-size' : inputsMapping.size.value,
-	    '@place' : this.getCheckedPlaceValue()       
+	    '@place' : this.getCheckedPlaceValue()
 	  });
-	}
+	  this.printVars();
+	};
+
+	this.printVars = function(){
+		document.getElementById("code-arrow-size").innerHTML = inputsMapping.size.value;
+		document.getElementById("code-border-color").innerHTML = inputsMapping.border_color.value;
+		document.getElementById("code-background-color").innerHTML = inputsMapping.color.value;
+		document.getElementById("code-border-width").innerHTML = inputsMapping.border_width.value;
+		document.getElementById("code-place").innerHTML = this.getCheckedPlaceValue();
+	};
+
 	this.modBox();
 }
 var lsscssrrwpls = new lesscssarrowplease();
